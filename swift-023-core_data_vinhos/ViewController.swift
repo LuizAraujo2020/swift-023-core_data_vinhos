f86fdebbb242a6ea90195d37ee3211e7e49e914a
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-25 22:17:10 -0300

    Initial Commit

---
+++ swift-023-core_data_vinhos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-023-core_data_vinhos
+//
+//  Created by Luiz Carlos da Silva Araujo on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
